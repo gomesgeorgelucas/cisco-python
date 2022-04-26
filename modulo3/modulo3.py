7d62315b4a05557c8dd6a8f88058337a9bc1e243
George Lucas Gomes <gomes.georgelucas@gmail.com>
2022-04-26 05:27:24 -0400

    loops and conditionals

--- modulo3/modulo3.py
+++ modulo3/modulo3.py
@@ -123,7 +123,254 @@
 #     guess = int(input("Guess and integer number: "))
 # print("Well done, muggle! You ae free now.")
 
+#
+# for i in range(100):
+#     # do_something()
+#     pass
 
-for i in range(100):
-    # do_something()
-    pass
+# nota: neste caso, a função range() começa o seu trabalho a partir do 0 e termina
+# um passo (um número inteiro) antes do valor do seu argumento;
+
+# range(início, limite superior não inclusivo, passo)
+for i in range(10):
+    print("The value of i is currently", i)
+print()
+for i in range(2, 8):
+    print("The value of i is currently", i)
+print()
+for i in range(2, 8, 3):
+    print("The value of i is currently", i)
+print()
+
+# for não quebra se o passo extrapolar o limite
+for i in range(3, 8, 3):
+    print("The value of i is currently", i)
+
+# Won't execute
+for i in range(1, 1):
+    print("The value of i is currently", i)
+
+# Nota: o conjunto gerado pelo range() tem de estar ordenado por ordem crescente.
+# Não há como forçar o range() a criar um conjunto de uma forma diferente quando
+# a função range() aceita exatamente dois argumentos. Isto significa que o segundo argumento
+# de range() deve ser maior que o primeiro.
+
+power = 1
+for expo in range(16):
+    print("2 to the power of", expo, "is", power)
+    power *= 2
+
+# import time
+
+# Write a for loop that counts to five.
+    # Body of the loop - print the loop iteration number and the word "Mississippi".
+    # Body of the loop - use: time.sleep(1)
+
+# Write a print function with the final message.
+# for count in range(1,6):
+#     print(str(count) + " Mississippi")
+#     time.sleep(1)
+# print("Ready or not, here I come!")
+
+# break - example
+
+print("The break instruction:")
+for i in range(1, 6):
+    if i == 3:
+        break
+    print("Inside the loop.", i)
+print("Outside the loop.")
+
+
+# continue - example
+
+print("\nThe continue instruction:")
+for i in range(1, 6):
+    if i == 3:
+        continue
+    print("Inside the loop.", i)
+print("Outside the loop.")
+
+# largest_number = -99999999
+# counter = 0
+#
+# while True:
+#     number = int(input("Enter a number or type -1 to end program: "))
+#     if number == -1:
+#         break
+#     counter += 1
+#     if number > largest_number:
+#         largest_number = number
+#
+# if counter != 0:
+#     print("The largest number is", largest_number)
+# else:
+#     print("You haven't entered any number.")
+
+# largest_number = -99999999
+# counter = 0
+#
+# number = int(input("Enter a number or type -1 to end program: "))
+#
+# while number != -1:
+#     if number == -1:
+#         continue
+#     counter += 1
+#
+#     if number > largest_number:
+#         largest_number = number
+#     number = int(input("Enter a number or type -1 to end program: "))
+#
+# if counter:
+#     print("The largest number is", largest_number)
+# else:
+#     print("You haven't entered any number.")
+
+# secret = "chupacabra"
+#
+# guess = input("Guess the word: ")
+#
+# while True:
+#     guess = input("Guess the word: ")
+#     if guess == secret:
+#         break
+# print("You've successfully left the loop")
+
+# Prompt the user to enter a word
+# and assign it to the user_word variable.
+# user_word = input("Vowel eater: ").upper()
+#
+# for letter in user_word:
+#     if letter == "A":
+#         continue
+#     elif letter == "E":
+#         continue
+#     elif letter == "I":
+#         continue
+#     elif letter == "O":
+#         continue
+#     elif letter == "U":
+#         continue
+#     else:
+#         print(letter)
+
+# user_word = input("Vowel eater: ").upper()
+#
+# word_without_vowels = ""
+#
+# for letter in user_word:
+#     if letter == "A":
+#         continue
+#     elif letter == "E":
+#         continue
+#     elif letter == "I":
+#         continue
+#     elif letter == "O":
+#         continue
+#     elif letter == "U":
+#         continue
+#     else:
+#         word_without_vowels += letter
+# print(word_without_vowels)
+
+# i = 1
+# while i < 5:
+#     print(i)
+#     i += 1
+# else:
+#     print("else:", i)
+
+# i = 5
+# while i < 5:
+#     print(i)
+#     i += 1
+# else:
+#     print("else:", i)
+
+# i = 1
+# while i < 5:
+#     print(i)
+#     break
+#     i += 1
+# else:
+#     print("else:", i)
+#
+# for i in range(5):
+#     print(i)
+# else:
+#     print("else:", i)
+
+# lu = 111
+# for lu in range(2, 1):
+#     print(lu)
+# else:
+#     print("else:", lu)
+
+# blocks = int(input("Enter the number of blocks: "))
+#
+# #
+# # Write your code here.
+# #
+#
+# height = 0
+# lineBricks = 0
+# if blocks >= 1:
+#     while True:
+#         lineBricks += 1
+#         if (blocks - lineBricks) == 0:
+#             height += 1
+#             break
+#         elif (blocks - lineBricks) >= (lineBricks + 1):
+#             blocks -= lineBricks
+#             height += 1
+#             continue
+#         else:
+#             height += 1
+#             break
+#
+# print("The height of the pyramid:", height)
+
+# c0 = int(input("c0 = "))
+# steps = 0
+# if c0 > 0:
+#     while True:
+#         if c0 % 2 == 0:
+#             c0 /= 2
+#             print("new c0 = " + str(int(c0)))
+#         else:
+#             c0 = 3 * c0 + 1
+#             print("new c0 = " + str(int(c0)))
+#         steps += 1
+#         if c0 != 1:
+#             continue
+#         else:
+#             break
+# else:
+#     print("Invalid integer! Must be a non-null integer.")
+#
+# print("final c0= " + str(int(c0)))
+# print("steps = " + str(steps))
+
+# Example 1
+word = "Python"
+for letter in word:
+    print(letter, end="*")
+print()
+# Example 2
+for i in range(1, 10):
+    if i % 2 == 0:
+        print(i)
+
+text = "OpenEDG Python Institute"
+for letter in text:
+    if letter == "P":
+        break
+    print(letter, end="")
+
+print()
+
+text = "pyxpyxpyx"
+for letter in text:
+    if letter == "x":
+        continue
+    print(letter, end="")
