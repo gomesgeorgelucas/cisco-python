77fe247e6aabbecf95796356a4828edaba245b5c
George Lucas Gomes <gomes.georgelucas@gmail.com>
2022-04-27 02:21:04 +0000

    Fim modulo 3

--- modulo3/modulo3.py
+++ modulo3/modulo3.py
@@ -701,3 +701,300 @@ print(my_list)
 print("Reverse list: ")
 my_list.reverse()
 print(my_list)
+
+#procedimentos do python de listas, alteram a lista inicial
+
+
+##listas podem ser passadas por referencia, diferente de variaveis comuns que sao passadas por valor
+
+# o nome de uma variavel comum eh o nome do seu coteudo
+
+#o nome de uma lista e o nome de um local de memoria onde a lista e armazenada, a copia da copia ainda eh apenas uma copia
+
+list_1 = [1]
+list_2 = list_1
+list_1[0] = 2
+print(list_2)
+
+# list_2 = list_1 copia o nome da referencia e não o conteudo
+
+print("Slice")
+
+# solucao para copiar valores
+# slice [start:end(non-inclusive)]
+
+# Copying the entire list.
+list_1 = [1]
+list_2 = list_1[:]
+list_1[0] = 2
+print(list_2)
+
+# Copying some part of the list.
+my_list = [10, 8, 6, 4, 2]
+new_list = my_list[1:3]
+print(new_list)
+
+# pode usar valores negativos do slice
+
+print("Slice negativo")
+
+my_list = [10,8,6,4,2]
+new_list = my_list[1:-1]
+print(new_list)
+
+my_list = [10,8,6,4,2]
+new_list = my_list[-1:1]
+print(new_list)
+
+# omitir o start do slice, assume que inicia do 0
+# se omitir o end do slice, assume que termina no ultimo elemento
+
+#utilizar del com slices
+
+print("Del com slices")
+
+my_list = [1,2,3,4,5,6]
+del my_list[1:3]
+print(my_list)
+del my_list[:]
+print(my_list)
+del my_list
+# del deleta a referencia para a lista
+# print(my_list) - gera erro de runtime
+
+print("in e not in")
+# funciona como no SQL
+
+# in - verifica se existe dentro da lista
+# not in - verifica se não existe na lista
+
+# in e not in retornam booleanos
+
+my_list = [0, 3, 12, 8, 2]
+
+print(5 in my_list)
+print(5 not in my_list)
+print(12 in my_list)
+
+my_list = [17, 3, 11, 5, 1, 9, 7, 15, 13]
+largest = my_list[0]
+
+for i in range(1, len(my_list)):
+    if my_list[i] > largest:
+        largest = my_list[i]
+
+print(largest)
+
+
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+to_find = 5
+found = False
+
+for i in range(len(my_list)):
+    found = my_list[i] == to_find
+    if found:
+        break
+
+if found:
+    print("Element found at index", i)
+else:
+    print("absent")
+
+drawn = [5, 11, 9, 42, 3, 49]
+bets = [3, 7, 11, 42, 34, 49]
+hits = 0
+
+for number in bets:
+    if number in drawn:
+        hits += 1
+
+print(hits)
+
+# challenge
+
+my_list = [1, 2, 4, 4, 1, 4, 2, 6, 2, 9]
+#
+# Write your code here.
+#
+
+my_list.sort()
+set = []
+
+for i in range(len(my_list)):
+  if my_list[i] not in set:
+    set.append(my_list[i])
+
+print("The list with unique elements only:")
+print(set)
+
+## List comprehension
+
+squares = [x ** 2 for x in range(10)]
+
+print(squares)
+
+twos = [2 ** i for i in range(8)]
+
+print(twos)
+
+odds = [x for x in squares if x % 2 != 0]
+
+print(odds)
+
+print("Tabuleiro")
+
+EMPTY = "-"
+ROOK = "ROOK"
+board = []
+
+# for i in range(8):
+#     row = [EMPTY for i in range(8)]
+#     board.append(row)
+
+board = [[EMPTY for i in range(8)] for j in range(8)]
+
+board[0][0] = ROOK
+board[0][7] = ROOK
+board[7][0] = ROOK
+board[7][7] = ROOK
+
+print(board)
+
+print("Meteorologia")
+# meteorologia
+temps = [[0.0 for h in range(24)] for d in range(31)]
+
+print(temps)
+
+total = 0.0
+for day in temps:
+  total += day[11]
+average = total / 31
+print("Average temperature at noon:",average)
+
+i=2
+while i >= 0:
+  print("*")
+  i-=2
+
+for i in range(-1,1):
+  print("#")
+
+print(10 > 0 or 10 == 0)
+
+l = [0,1,2]
+# l[-1] = l[-2]
+# print(l)
+
+l[0], l[1] = l[1],l[2]
+print(l)
+
+n = []
+h = n
+h.append(1)
+
+print(n,h)
+
+print([0 for i in range(1,3)])
+
+i = [0,1,2,3]
+x = 1
+
+for el in i:
+  x *= el
+print(x)
+
+a = [1,2,3]
+b = []
+
+for v in a:
+  b.insert(0,v)
+print(b)
+
+l =[3,1,-2]
+l.insert(0,1)
+del l[1]
+print(l)
+
+for i in range(1):
+  print("y")
+else:
+  print("y")
+
+print(l[l[-1]])
+
+t = [[3-1 for i in range(3)] for j in range(3)]
+s = 0
+
+for i in range(3):
+  s+=t[i][i]
+print(s)
+
+i = 0
+while i<=3 :
+  i+=2
+  print("*")
+
+nums=[1,2,3]
+vals=nums[-1:-2]
+print(nums,vals)
+
+var = 0
+while var < 6:
+  var += 1
+  if var % 2 == 0:
+    continue
+  print("#")
+
+print(0 < 10 and 10 > 0 or 0 > 10 and 10 < 0)
+
+a = 1
+b = 0
+c = a & b
+d = a | b
+e = a ^ b
+
+print(c + d + e)
+
+var =1 
+while var < 10:
+  print("$")
+  var = var << 1
+
+i = 0
+while i<=5:
+  i+=1
+  if i %2 ==0:
+    break
+  print("*")
+
+m = [[0,1,2,3] for i in range(2)]
+
+print(m[0][2])
+
+nums = [1,2,3]
+vals =nums
+del vals[1:2]
+
+print(nums,vals)
+
+x = 1
+x = x == x
+print(x)
+
+l = [0,1,2]
+l[0], l[2] = l[2], l[0]
+print(l)
+print(len([i for i in range(-1,2)]))
+
+print([i for i in range(-1,2)])
+
+my = [1,2,3]
+for v in range(len(my)):
+  my.insert(1,my[v])
+print(my)
+
+
+m = [1,2,3,4]
+print(m[-3:-2])
+
